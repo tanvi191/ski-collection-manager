1a7c1c7caef308cdc302570a37726079adef95b2
Tanvi <tanvidav6@gmail.com>
2025-01-09 13:42:55 +0000

    Defining all column variables from dataset

---
+++ src/types/SkiResort.ts
@@ -0,0 +1,19 @@
+export interface SkiResort {
+    BeginnerSlope: number;
+    ChairLifts: number;
+    Country: string;
+    DayPassPriceAdult: number;
+    DifficultSlope: number;
+    HighestPoint: number;
+    IntermediateSlope: number;
+    LiftCapacity: number;
+    LowestPoint: number;
+    NightSki: boolean;
+    Resort: string;
+    SnowCannons: number;
+    SnowParks: boolean;
+    SurfaceLifts: number;
+    TotalLifts: number;
+    TotalSlope: number;
+    id: number;
+}
