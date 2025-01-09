60509e1861e12cb9aa1d932206117303c7761026
Tanvi <tanvidav6@gmail.com>
2025-01-09 14:42:08 +0000

    Fetched the CSV data

---
+++ src/services/SkiResortService.ts
@@ -0,0 +1,18 @@
+import { SkiResort } from '../types/SkiResort';
+
+class SkiResortService {
+    private skiResorts: SkiResort[] = [];
+
+    async fetchSkiResorts(): Promise<SkiResort[]>{
+        try {
+            const response = await fetch('https://hebbkx1anhila5yf.public.blob.vercel-storage.com/European_Ski_Resorts-MMmjLiKOclX0IX3CziLyR7BM1s2gJ9.csv');
+            const csvData = await response.text();
+            this.skiResorts = this.parseCSV(csvData);
+            return this.skiResorts;
+        } catch (error) {
+            console.error('Error fetching ski resorts: ', error);
+            return [];
+        }
+        }
+
+}
